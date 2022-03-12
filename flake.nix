4817b809ff74b9a9d861fdc0161d8f1ead73d2f9
hugosenari <hugosenari@gmail.com>
2022-03-12 04:26:13 -0300

    chore: re index nimclutter

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim bindings for Clutter toolkit.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimclutter-master".url = "path:./master";
+  inputs."nimclutter-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimclutter-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
